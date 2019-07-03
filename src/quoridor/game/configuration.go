ffc5b6138a7dfeabce8463de80b2d3e4bcb709fb
Matthieu Chaffotte <matthieu.chaffotte@gmail.com>
2019-07-03 17:20:04 +0200

    fix json decoder configuration

--- src/quoridor/game/configuration.go
+++ src/quoridor/game/configuration.go
@@ -3,5 +3,5 @@ package game
 //Configuration options to create a game
 type Configuration struct {
 	BoardSize int `json:boardSize`
-	NumberOfFencesPerPawnPlayer int `json:numberOfFencesPerPlayer`
+	NumberOfFencesPerPlayer int `json:numberOfFencesPerPlayer`
 }
